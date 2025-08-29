844ae19198243344e03606268d35b3c42f63535f
vivek <vivekcse.in@gmail.com>
2025-08-29 18:26:54 +0530

    signup page restricted if loggedin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,16 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { SigninForm } from "@/components/forms/auth.main";
+import { useAuth } from "@/components/providers/AuthContext";
 
 const SigninPage = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
+  if (isAuthenticated) {
+    router.push("/");
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Link
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,16 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { SignupForm } from "@/components/forms/auth.main";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/components/providers/AuthContext";
 
 const SignupPage = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useAuth();
+  if (isAuthenticated) {
+    router.push("/");
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 ">
       <Link
